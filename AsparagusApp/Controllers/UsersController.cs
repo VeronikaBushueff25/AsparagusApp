9286744c20983e2394b63c6d3d80a45110c28801
Вероника Бушуева <fucking_shit25@mail.ru>
2024-07-09 20:28:05 +0700

    Добавьте файлы проекта.

---
+++ AsparagusApp/Controllers/UsersController.cs
@@ -0,0 +1,59 @@
+﻿using AsparagusApp.Data;
+using AsparagusApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsparagusApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            var existingUser = await _context.users.FirstOrDefaultAsync(u => u.email == user.email);
+
+            if (existingUser != null)
+            {
+                existingUser.name = user.name;
+                existingUser.asparaguscount++;
+                existingUser.lastasparagusdate = DateTime.UtcNow;
+                await _context.SaveChangesAsync();
+                return Ok(existingUser);
+            }
+
+            user.asparaguscount = 1;
+            user.lastasparagusdate = DateTime.UtcNow;
+            _context.users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUser), new { id = user.id }, user);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.users.OrderByDescending(u => u.lastasparagusdate).ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+    }
+}
---
+++ AsparagusApp/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using AsparagusApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsparagusApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<User> users { get; set; }
+    }
+}
